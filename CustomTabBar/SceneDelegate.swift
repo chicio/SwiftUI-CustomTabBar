f37c69c003227c8634d3dbb9d2ef37e630130847
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2020-03-06 12:18:13 +0100

    Refactoring :recycle:

--- CustomTabBar/SceneDelegate.swift
+++ CustomTabBar/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let contentView = CustomTabBar()
+        let contentView = MainView()
 
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
